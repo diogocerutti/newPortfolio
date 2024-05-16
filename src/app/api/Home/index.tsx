a382c5c9bedda6bfbf32a17adae4c1fd6aa7bd12
diogocerutti <diogo.cerutti19@gmail.com>
2024-05-16 15:06:07 -0300

    homePage initials

---
+++ src/app/api/Home/index.tsx
@@ -0,0 +1,19 @@
+export const headerItems: Array<string> = ["Projects", "CV"];
+
+interface centerInfo {
+  name?: string;
+  ocuppation?: string;
+  description?: string;
+  subDescription?: string;
+  github?: string;
+  linkedin?: string;
+}
+
+export const centerInfo: centerInfo = {
+  name: "Diogo Cerutti",
+  ocuppation: "Data Migration Assistant",
+  description: "lets gooooooooooooooooo",
+  subDescription: "oioioi",
+  github: "https://github.com/diogocerutti",
+  linkedin: "https://www.linkedin.com/in/diogo-cerutti/",
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import styles from "./page.module.css";
-import { headerItems, admOptions } from "./api/homeHeader";
+import { headerItems, centerInfo } from "./api/Home";
 import { useState } from "react";
-import logoAmpli from "./images/Logo_novo.png";
+import homeIcon from "./images/home-w.png";
+import githubIcon from "./images/github-w.png";
+import linkedinIcon from "./images/linkedin-w.png";
 
 export default function Home() {
   const [dropdownAdm, setDropdownAdm] = useState("none");
@@ -12,78 +14,53 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.header}>
+        <a className={styles.headerButton} href="#">
+          <img className={styles.homeIcon} src={homeIcon.src} />
+        </a>
         <div className={styles.headerItems}>
-          <div
-            style={{
-              display: "flex",
-              columnGap: 20,
-            }}
-          >
-            <img className={styles.logoAmpli} src={logoAmpli.src} />
-            {headerItems.map((row, index) => {
-              if (row !== "Administração" && row !== "Configurações") {
-                return (
-                  <a key={index} href="#" className={styles.headerButton}>
-                    {row}
-                  </a>
-                );
-              }
-              if (row == "Administração") {
-                return (
-                  <div
-                    key={index}
-                    style={{ display: "flex", flexDirection: "column" }}
-                  >
-                    <a
-                      href="#"
-                      className={styles.headerButton}
-                      onMouseEnter={() => setDropdownAdm("flex")}
-                      onMouseLeave={() => setDropdownAdm("none")}
-                    >
-                      {row}⬇️
-                    </a>
-                    <div
-                      style={{ display: dropdownAdm, flexDirection: "column" }}
-                    >
-                      {admOptions.map((r, i) => (
-                        <div>{r}</div>
-                      ))}
-                    </div>
-                  </div>
-                );
-              } else if (row == "Configurações") {
-                return (
-                  <div
-                    key={index}
-                    style={{ display: "flex", flexDirection: "column" }}
-                  >
-                    <a
-                      href="#"
-                      className={styles.headerButton}
-                      onMouseEnter={() => setDropdownConfig("flex")}
-                      onMouseLeave={() => setDropdownConfig("none")}
-                    >
-                      {row}⬇️
-                    </a>
-                    <div
-                      className={styles.headerDropdown}
-                      style={{ display: dropdownConfig }}
-                    >
-                      config
-                    </div>
-                  </div>
-                );
-              }
-            })}
-          </div>
-          <div
-            style={{
-              justifyContent: "space-between",
-              padding: "15px 10px 15px 10px",
-            }}
-          >
-            🔔🗣️
-          </div>
+          {headerItems.map((row, index) => {
+            if (row !== "Administração" && row !== "Configurações") {
+              return (
+                <a key={index} href="#" className={styles.headerButton}>
+                  {row}
+                </a>
+              );
+            }
+          })}
+        </div>
+      </div>
+      <div className="centerInfo">
+        <div style={{ fontSize: "48px" }}>{centerInfo.name}</div>
+        <div style={{ marginTop: "0.5rem", fontSize: "30px" }}>
+          {centerInfo.ocuppation}
+        </div>
+        <div
+          className="home-description"
+          style={{
+            marginTop: "3rem",
+            textAlign: "justify",
+            textJustify: "inter-word",
+          }}
+        >
+          {centerInfo.description}
+        </div>
+        <div
+          className="homeDescription"
+          style={{
+            marginTop: "1rem",
+            textAlign: "justify",
+            textJustify: "inter-word",
+          }}
+        >
+          {centerInfo.subDescription}
+        </div>
+        <div style={{ marginTop: "3rem" }}>
+          <a href={centerInfo.github}>
+            <img src={githubIcon.src} />
+          </a>
+          <a href={centerInfo.linkedin}>
+            <img src={linkedinIcon.src} />
+          </a>
         </div>
       </div>
     </main>
