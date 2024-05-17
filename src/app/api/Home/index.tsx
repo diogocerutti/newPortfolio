3f1e214656c5033c48aa35f7d00226d02542b5ff
Diogo Cerutti <83303702+diogocerutti@users.noreply.github.com>
2024-05-17 10:54:44 -0300

    First commit

---
+++ src/app/api/Home/index.tsx
@@ -0,0 +1,20 @@
+export const headerItems: Array<string> = ["Projects", "CV"];
+
+interface centerInfo {
+  name?: string;
+  ocuppation?: string;
+  description?: string;
+  subDescription?: string;
+  github?: string;
+  linkedin?: string;
+}
+
+export const centerInfo: centerInfo = {
+  name: "Diogo Cerutti",
+  ocuppation: "Data Migration Assistant",
+  description:
+    "Welcome! My name is Diogo, I'm 21 years old and I live in Chapecó city, in Santa Catarina, Brazil.",
+  subDescription: "oioioi",
+  github: "https://github.com/diogocerutti",
+  linkedin: "https://www.linkedin.com/in/diogo-cerutti/",
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import styles from "./page.module.css";
+import { headerItems, centerInfo } from "./api/Home";
+import { useState } from "react";
+import homeIcon from "./images/home-w.png";
+import githubIcon from "./images/github-w.png";
+import linkedinIcon from "./images/linkedin-w.png";
+
+export default function Home() {
+  const [dropdownAdm, setDropdownAdm] = useState("none");
+  const [dropdownConfig, setDropdownConfig] = useState("none");
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.wrap}>
+        <div className={styles.header}>
+          <a className={styles.headerButton} href="#">
+            <img className={styles.homeIcon} src={homeIcon.src} />
+          </a>
+          <div className={styles.headerItems}>
+            {headerItems.map((row, index) => {
+              if (row !== "Administração" && row !== "Configurações") {
+                return (
+                  <a key={index} href="#" className={styles.headerButton}>
+                    {row}
+                  </a>
+                );
+              }
+            })}
+          </div>
+        </div>
+        <div className="centerInfo">
+          <div style={{ fontSize: "48px" }}>{centerInfo.name}</div>
+          <div style={{ marginTop: "0.5rem", fontSize: "30px" }}>
+            {centerInfo.ocuppation}
+          </div>
+          <div
+            style={{
+              marginTop: "3rem",
+              textAlign: "justify",
+              textJustify: "inter-word",
+            }}
+          >
+            {centerInfo.description}
+          </div>
+          <div
+            style={{
+              marginTop: "1rem",
+              textAlign: "justify",
+              textJustify: "inter-word",
+            }}
+          >
+            {centerInfo.subDescription}
+          </div>
+          <div style={{ marginTop: "3rem" }}>
+            <a href={centerInfo.github}>
+              <img src={githubIcon.src} />
+            </a>
+            <a href={centerInfo.linkedin}>
+              <img src={linkedinIcon.src} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
