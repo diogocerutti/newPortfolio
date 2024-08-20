6472742ed35ab87b4e8558f2dcb5e0f5aa9e67e1
diogocerutti <diogo.cerutti19@gmail.com>
2024-08-20 13:57:27 -0300

    New config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/cerutti-portfolio",
-  output: "export", // <=== enables static exports
-  reactStrictMode: true,
+  output: "export",
+  assetPrefix: "./", // necessário para que os recursos sejam carregados corretamente
+  images: {
+    unoptimized: true, // necessário para desabilitar a otimização de imagens no Next.js
+  },
+  basePath: "/diogocerutti.github.io", // substitua "NOME_DO_REPOSITÓRIO" pelo nome do seu repositório no GitHub
 };
 
 export default nextConfig;
