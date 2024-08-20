9e9c041f789f4cee84b7462f8ba387501cbb00da
diogocerutti <diogo.cerutti19@gmail.com>
2024-08-20 14:11:05 -0300

    Novo deploy

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,10 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   assetPrefix: isProd ? "/diogocerutti.github.io/" : "",
+  basePath: isProd ? "/diogocerutti.github.io" : "",
   images: {
     unoptimized: true, // necessário para desabilitar a otimização de imagens no Next.js
   },
-  basePath: isProd ? "/diogocerutti.github.io" : "",
 };
 
 export default nextConfig;
