b679d7b09dfb4cb9eafbab02a5fadd14cd301a71
diogocerutti <diogo.cerutti19@gmail.com>
2024-08-20 14:03:25 -0300

    Deploy Next

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
-  assetPrefix: "./", // necessário para que os recursos sejam carregados corretamente
+  assetPrefix: isProd ? "/diogocerutti.github.io/" : "",
   images: {
     unoptimized: true, // necessário para desabilitar a otimização de imagens no Next.js
   },
-  basePath: "/diogocerutti.github.io", // substitua "NOME_DO_REPOSITÓRIO" pelo nome do seu repositório no GitHub
+  basePath: isProd ? "/diogocerutti.github.io" : "",
 };
 
 export default nextConfig;
